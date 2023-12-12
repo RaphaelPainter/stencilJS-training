89555d96d87f57cfbf70f3ef6d5208d2a998b9a2
RaphaelPainter <raphael.painter@imt-atlantique.net>
2023-12-12 09:55:57 +0100

    init first component

---
+++ stencil-demo-project/src/components/my-card/my-card.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'my-card',
+    styleUrl: 'my-card.css',
+    shadow: true,
+})
+
+export class MyCard {
+    render(){
+        return(<div class="my-card-wrapper">This is my card component.</div>)
+    }
+}
